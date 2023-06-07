bb90df8cc8160cd9b1fb5ef006199fd51a0e250c
Ge Su <gegege.su@gmail.com>
2023-06-07 10:33:18 +1000

    added switch function & delete query file

--- index.js
+++ index.js
@@ -28,35 +28,113 @@ const initQuestion = [{
     type: 'list',
     name: 'menu',
     message: 'What would you like to do?',
-    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Departments', 'Quit'],
+    choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
 }];
+
+//View All Departments
+const viewDepartments = () => {
+    const sql =`SELECT * FROM departments`
+    db.query(sql, (err, results) => {
+        err ? console.log(err) : console.table(results)
+        //console.log(results);
+        init();
+    })
+};
+
+//View All Roles
+const viewRoles = () => {
+    const sql =`SELECT * FROM roles`
+    db.query(sql, (err, results) => {
+        err ? console.log(err) : console.table(results)
+        //console.(results);
+        init();
+    })
+};
+
+//View All Employees
+const viewEmployees = () => {
+    const sql =`SELECT * FROM employees`
+    db.query(sql, (err, results) => {
+        err ? console.log(err) : console.table(results)
+        //console.log(results);
+        init();
+    })
+};
+
+
 //add department
 
-const addDepartment = [
-{
-    type: 'input',
-    name: 'department',
-    message: 'What is the name of the department?',
-}];
-//add role
+const addDepartment = () => {
+    inquirer
+    .prompt([{
+        type: 'input',
+        name: 'department',
+        message: 'What is the name of the department?',
+    }])
+    .then((answer)=>{
+        const sql =`INSERT INTO departments (department_name) VALUES (?)`
+        db.query(sql, answer.department, (err, results) => {
+        if (err) {
+            console.log(err)
+        } else {
+            //console.table(results)
+            //console.log(results);
+            viewDepartments()
+            init();
+        } 
+      })
+    })
+};
 
-const addRole = [
-{
-    type: 'input',
-    name: 'role',
-    message: 'What is the name of the role?',
-},
-{
-    type: 'input',
-    name: 'salary',
-    message: 'What is the salary of the role?',
-},
-{
-    type: 'list',
-    name: 'department',
-    message: 'Which department does the role belong to?',
-    choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service',]
-}];
+//add role
+const addRole = () => {
+    const getDepartments = () =>
+        db.promise().query(`SELECT * FROM departments`)
+        .then((results) => {
+            //console.log(results)
+            const departArr = results[0].map((obj) => obj.department_name);
+            //console.log(departArr);
+            return departArr;
+        })   
+    
+
+
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the role?',
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary of the role?',
+    },
+    {
+        type: 'list',
+        name: 'department',
+        message: 'Which department does the role belong to?',
+        choices: getDepartments
+    }
+    ]).then(ans => {
+        db.promise().query(`SELECT id FROM departments WHERE department_name = ?`, ans.department)
+            .then(result => {
+                console.log(result)
+                let departmentId = result[0].map(obj => obj.id);
+                console.log(departmentId[0])
+                //console.log(departmentId[0])
+                return departmentId[0]
+            })
+            .then((departmentId) => {
+                db.promise().query(`INSERT INTO roles(title, salary, department_id)
+            VALUES(?, ?, ?)`, [ans.title, ans.salary, departmentId]);
+                init()
+            })
+    })
+
+};
+ 
 // add employee
 const addEmployee = [
 {
@@ -94,32 +172,28 @@ const updateRole = [
 function init() {
     inquirer
     .prompt(initQuestion)
-    .then((answer) => {
-        //console.log(answer);
-        //console.log(answer.menu);
-        //let selection = answer.manu
-        if(answer.menu === 'View All Employees') {
-            const sql ='SELECT * FROM employees'
-            db.query(sql, (err, results) => {
-                 console.table(results)
-                 return init();
-                  });
-        } else if(answer.menu === 'View All Roles') {
-            const sql ='SELECT * FROM roles'
-            db.query(sql, (err, results) => {
-                console.table(results)
-                 return init();
-                  });
-        } else if(answer.menu === 'View All Departments') {
-            const sql ='SELECT * FROM departments'
-            db.query(sql, (err, results) => {
-                console.table(results)
-                 return init();
-                  });
-        };     
-
-    }) 
-    
+    .then(answer => {
+     switch (answer.menu) {
+        case 'View All Departments': viewDepartments();
+        break;
+        case 'View All Roles': viewRoles();
+        break;
+        case 'View All Employees': viewEmployees();
+        break;
+        case 'Add Department': addDepartment();
+        break;
+        case 'Add Role': addRole();
+        break;
+        case 'Add Employee': addEmployee();
+        break;
+        case 'Update Employee Role': updateRole();
+        break;
+        case 'Quit':
+            process.exit();
+     }
+    })
+    .catch(err => console.error(err)); 
 }
 // Function call to initialize app
 init();
+
